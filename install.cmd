8e18594eb7e8fd75ac3b2b02782ae21e6cd1a78b
Ori Levi <ori1991@gmail.com>
2017-04-08 17:19:39 +0300

    finalize the install.cmd script

--- install.cmd
+++ install.cmd
@@ -2,18 +2,6 @@
 @cd /d "%~dp0"
 @echo off
 
-::echo Administrative permissions required.
-::<nul set /p ".=Detecting permissions..."
-::net session >nul 2>&1
-::if NOT %errorLevel% == 0 (
-::	echo [E R R O R]
-::	echo Please Run As Admin!
-::	pause
-::	goto:eof
-::) else (
-::	echo [O K]
-::)
-
 :InitState
 	cls
 	Title Gvahim Package Installer - v1.0 & Color 0A
@@ -28,18 +16,6 @@
 		call:Display "Administrative permissions required." "Please Run As Admin" "[e] ERROR !!!" %sstage%
 		goto:eof
     )
-
-:removeOldInstall
-    set /a sstage+=1
-    call:Display "Uinstall old gvahim installation" "Uninstalling..." "[i] INFO" %sstage%
-    "C:\Heights\PortableApps\InitialSetup\untested_uninstall.bat"
-    rmdir /S /Q "C:\Heights\PortableApps\"
-    del "C:\Heights\Start.exe"
-    if exist "C:\Heights\first.bat" (
-        del "C:\Heights\first.*"
-    )
-    echo Delete Heights folder, keep Documents folder (C:\Heights\Documents)
-    net session >nul 2>&1
 
 :pythonInstall
     set /a sstage+=1
@@ -82,23 +58,3 @@
 	rem pause
 	PING -n 2 127.0.0.1 > nul
 	goto:eof
-
-
-
-
-
-
-::set PYTHON_DIR=%cd%\python27
-::set INSTALLATION_DIR=%cd%\installation
-::
-::<nul set /p ".=Installing python 2.7.13		"
-::msiexec /i "%INSTALLATION_DIR%\python\python-2.7.13.msi" /quiet /passive TARGETDIR=%PYTHON_DIR% ADDLOCAL=ALL
-::echo [D O N E]
-::
-::cls
-::python "%INSTALLATION_DIR%\installer.py"
-::
-::rmdir /S /Q %INSTALLATION_DIR%
-::del install.cmd
-
-pause
