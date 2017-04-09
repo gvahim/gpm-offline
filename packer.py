815b4fa48a174a52d2916d5d871649d34d08f251
Ori Levi <ori1991@gmail.com>
2017-04-09 04:02:23 +0300

    verify the downloads

--- packer.py
+++ packer.py
@@ -2,6 +2,7 @@ import os
 import pip
 import shutil
 import urllib
+import hashlib
 import argparse
 from colorama import init, Fore
 from contextlib import contextmanager
@@ -19,19 +20,39 @@ PACKAGES = {
 }
 
 GENERAL_SOFTWARES = {
-    'WinPcap': 'https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe',
-    'PyCharm': 'https://download.jetbrains.com/python/pycharm-community-2017.1.exe',
-    'VCForPython27': 'https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
+    'WinPcap': (
+        'https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe',
+        'fc4623b113a1f603c0d9ad5f83130bd6de1c62b973be9892305132389c8588de'
+    ),
+    'PyCharm': (
+        'https://download.jetbrains.com/python/pycharm-community-2017.1.exe',
+        '92bdb017ea1b6f8cff0c454ba44bf7de04f61f443b4897919e0202f56f863814'
+    ),
+    'VCForPython27': (
+        'https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi',
+        '070474db76a2e625513a5835df4595df9324d820f9cc97eab2a596dcbc2f5cbf')
 }
 
 SOFTWARES_32BIT = {
-    'Wireshark': 'https://1.as.dl.wireshark.org/win32/Wireshark-win32-2.2.5.exe',
-    'python': 'https://www.python.org/ftp/python/2.7.13/python-2.7.13.msi',
+    'Wireshark': (
+        'https://1.as.dl.wireshark.org/win32/Wireshark-win32-2.2.5.exe',
+        'a99a8cc1df24b31ab9ad963c2d1133982e0e9f2b33e8bfab8ac313c16e432da3'
+    ),
+    'python': (
+        'https://www.python.org/ftp/python/2.7.13/python-2.7.13.msi',
+        '44ea95356365195b18a5058796285789b0bfc94da1ee2ec1cb8e0a1c2ff6017a'
+    )
 }
 
 SOFTWARES_64BIT = {
-    'Wireshark': 'https://1.as.dl.wireshark.org/win64/Wireshark-win64-2.2.5.exe',
-    'python': 'https://www.python.org/ftp/python/2.7.13/python-2.7.13.amd64.msi',
+    'Wireshark': (
+        'https://1.as.dl.wireshark.org/win64/Wireshark-win64-2.2.5.exe',
+        'ab2723ba25dcf1e2f60faa579c1cad3e88ebcf53cf1a2a6897094b9f447fb864'
+    ),
+    'python': (
+        'https://www.python.org/ftp/python/2.7.13/python-2.7.13.amd64.msi',
+        '8b3e65fc1aad8809bb69477e922c3609a8e8fa9e2f6d5ab8f00f3553e3c61d7a'
+    )
 }
 
 
@@ -76,12 +97,18 @@ def download_file(name, url):
     save_path = os.path.join(SOFTWARES_DIR, '{}{}'.format(name, file_extension))
     urllib.urlretrieve(url, save_path, report_download)
     print
+    return save_path
 
 
 def create_empty_directory(path):
     if os.path.exists(path):
         shutil.rmtree(path)
     os.mkdir(path)
+
+
+def calculate_sha256(path):
+    with open(path, 'rb') as f:
+        return hashlib.sha256(f.read()).digest()
 
 if __name__ == '__main__':
     init(autoreset=True)
@@ -114,5 +141,14 @@ if __name__ == '__main__':
         softwares.append(SOFTWARES_32BIT)
 
     for software in softwares:
-        for name_, url_ in software.iteritems():
-            download_file(name_, url_)
+        for name_, (url_, hash_) in software.iteritems():
+            path = download_file(name_, url_)
+            calc_hash = calculate_sha256(path)
+            if hash_ == calc_hash:
+                color = Fore.GREEN
+                msg = 'O K'
+            else:
+                color = Fore.RED
+                msg = 'E R R O R'
+            msg_ = '{} - Verify download ... '.format(name_)
+            print '{}{}[{}]'.format(fix_width(msg_), color, msg)
