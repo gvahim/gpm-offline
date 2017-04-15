51b5203fc8ee44d100649ee1d53d5d8eb4741d5a
Ori Levi <ori1991@gmail.com>
2017-04-16 00:45:34 +0300

    steps.py:   - add code to fix scapy for windows

--- installation/installer/steps.py
+++ installation/installer/steps.py
@@ -115,6 +115,7 @@ def install_python_packages():
 def install_networks_packages():
     packages_file = os.path.join(INSTALLATION_DIR, 'networks.packages')
     install_with_pip(packages_file, 'Python Package for Networks')
+    __fix_scapy_imports()
 
 
 def install_winpcap():
@@ -216,6 +217,38 @@ def test_everything_is_good():
 
     print '{}YAY everything is install! have fun'.format(Fore.LIGHTRED_EX)
     raw_input('Press any key to continue...')
+
+
+def __fix_scapy_imports():
+    with notifier('scapy', 'Fixing'):
+        file_path = os.path.join(os.getcwd(), 'python27', 'lib',
+                                 'site-packages', 'scapy', 'arch',
+                                 'windows', 'compatibility.py')
+
+        with open(file_path, 'r+') as f:
+            addition = [
+                "from scapy.base_classes import Gen, SetGen", os.linesep,
+                "import scapy.plist as plist", os.linesep,
+                "from scapy.utils import PcapReader", os.linesep,
+                "from scapy.data import MTU, ETH_P_ARP", os.linesep,
+                "import os, re, sys, socket, time, itertools", os.linesep,
+                "WINDOWS = True", os.linesep
+            ]
+
+            lines = f.readlines()
+            index = 0
+            found = 0
+            for line in lines:
+                if line.startswith('import') or line.startswith('from'):
+                    found += 1
+                index += 1
+
+                if found == 2:
+                    break
+
+            lines = lines[:index] + addition + lines[index:]
+            f.seek(0, 0)
+            f.writelines(lines)
 
 
 if __name__ == '__main__':
