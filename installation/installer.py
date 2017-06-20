19cc9fdf0d7fae9d70953d4c8f7b3c9291c9e5dd
Ori Levi <ori1991@gmail.com>
2017-06-20 18:29:55 +0300

    increment the version

--- installation/installer.py
+++ installation/installer.py
@@ -6,7 +6,7 @@ from colorama import init
 from installer.steps import *
 from installer.display import display
 
-INSTALLER_VERSION = '1.2'
+INSTALLER_VERSION = '1.3'
 INSTALLER_TITLE = 'Gvahim Package Installer - v{}'.format(INSTALLER_VERSION)
 
 STEPS = (
